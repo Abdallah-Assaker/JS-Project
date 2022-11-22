8b9f5ffbbe1bb4ce2c279044ba212f758b0ff2a3
Abdallah Assaker <abdallahassaker@gmail.com>
2022-11-22 19:01:37 +0200

    Finall Main.JS File For Doll

--- main.js
+++ main.js
@@ -1,37 +1,115 @@
-var thePath = document.querySelectorAll("g");
-console.log(thePath);
+// var thePath = document.querySelectorAll("g");
+// console.log(thePath);
 
 var classes = [
 	".Hair",
 	".Eye",
-	"Nose",
+	".Nose",
 	".Mouth",
 	".EarsLeft",
 	".EarsRight",
 	".rightLeg",
-    ".leftLeg",
-    ".Neck",
-    ".Face",
-    ".Clothes",
-    ".Hand",
-    ".Teddy",
-    ".brow"
+	".leftLeg",
+	".Neck",
+	".Face",
+	".Clothes",
+	".Hand",
+	".Teddy",
+	".brow",
 ];
 
 for (let i = 0; i < classes.length; i++) {
-    var classArr = document.querySelectorAll(classes[i])
-    for (let j = 0; j < array.length; j++) {
-        classArr[j].addEventListener("click", Display)
-    }
+	var classArr = document.querySelectorAll(classes[i]);
+	// console.log(classArr)
+	for (let j = 0; j < classArr.length; j++) {
+		classArr[j].addEventListener("click", Display);
+	}
 }
 
+function Display() {
+	// console.log(event.target.getAttribute("class"));
+	// console.log(event.target)
+	var word;
+	switch (event.target.getAttribute("class")) {
+		case "Hair":
+			word = "Hair";
+            document.getElementById("hairSound").play();
+			break;
+		case "Eye":
+			word = "Eye";
+            document.getElementById("eyeSound").play();
+			break;
+		case "Nose":
+			word = "Nose";
+            document.getElementById("noseSound").play();
+			break;
+		case "Mouth":
+			word = "Mouth";
+            document.getElementById("mouthSound").play();
+			break;
+		case "EarsLeft":
+			word = "Left Ear";
+            document.getElementById("leftEarSound").play();
+			break;
+		case "EarsRight":
+			word = "Right Ear";
+            document.getElementById("rightEarSound").play();
+			break;
+		case "rightLeg":
+			word = "Right Leg";
+            document.getElementById("rightLegSound").play();
+			break;
+		case "leftLeg":
+			word = "Left Leg";
+            document.getElementById("leftLegSound").play();
+			break;
+		case "Neck":
+			word = "Neck";
+            document.getElementById("NeckSound").play();
+			break;
+		case "Face":
+			word = "Face";
+            document.getElementById("FaceSound").play();
+			break;
+		case "Clothes":
+			word = "Clothes";
+            document.getElementById("ClothesSound").play();
+			break;
+		case "Hand":
+			word = "Hand";
+            document.getElementById("HandSound").play();
+			break;
+		case "Teddy":
+			word = "Teddy Bear";
+            document.getElementById("TeddySound").play();
+			break;
+		case "brow":
+			word = "Eye Brow";
+            document.getElementById("browSound").play();
+			break;
+	}
+    console.log()
+    show(word)
+}
 
+var wordDiv = document.getElementById("Word");
+function show(input) {
+	wordDiv.innerHTML = input;	
+	FadeIn(wordDiv)
+}
 
-
-
-// for (let i = 0; i < thePath.length; i++) {
-//     for (let j = 0; j < thePath[i].childNodes.length; j++) {
-//         thePath[i][j].add
-//     }
-
-// }
+function FadeIn(ele) {
+	var i = 0;
+	var stop = setInterval(
+		function (ele) {
+			ele.style = `opacity : ${i}`;
+			if (i <= 1) {
+				i += 0.1;
+			} else {
+				clearInterval(stop);
+			}
+		},
+		50,
+		ele
+	);
+}
