999abfe247cbde9e124bf99f3a88518cf0dd30e2
Abdallah Assaker <abdallahassaker@gmail.com>
2022-11-22 17:11:26 +0200

    Doll

---
+++ main.js
@@ -0,0 +1,12 @@
+var thePath = document.querySelectorAll("g")
+console.log(thePath);
+
+var classes = [".Hair", ".Eye", "Nose"]
+
+
+// for (let i = 0; i < thePath.length; i++) {
+//     for (let j = 0; j < thePath[i].childNodes.length; j++) {
+//         thePath[i][j].add
+//     }
+    
+// }
